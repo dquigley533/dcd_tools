1928f6c78873bd8b291f2091d3469a26c0d200c2
dquigley-warwick <d.quigley@warwick.ac.uk>
2019-12-04 17:56:10 +0000

    Read complete header

--- dcd_reader.py
+++ dcd_reader.py
@@ -6,18 +6,29 @@ import numpy as np
 class dcd_trajectory:
     """A class representing a trajectory inside a dcd file."""
 
+    num_atoms = np.int32()
+    dcd_title = str()
+
     def __init__(self, dcdfile):
         """Define initial state."""
 
         # Read the dcd header as a sequence of FORTRAN records
 
-        # 1. Should be 4 characters "CORD" + 20 32-bit integers as icntrl array
+        # Record 1 should be 4 characters "CORD" + 20 32-bit integers as icntrl array
         hdr_bytes = self.read_next_fortran_record(dcdfile)
         hdr = hdr_bytes[0:4].decode('utf-8')
 
         # icntrl array
         icntrl_bytes = hdr_bytes[4:len(hdr_bytes)]  
         icntrl = np.frombuffer(icntrl_bytes, dtype=np.int32, count=20)
+
+        # Record 2 is the dcd title (80 bytes)
+        hdr_bytes = self.read_next_fortran_record(dcdfile)
+        self.dcd_title = hdr_bytes.decode()
+
+        # Record 3 is the number of atoms in each snapshot
+        hdr_bytes = self.read_next_fortran_record(dcdfile)
+        self.num_atoms = np.frombuffer(hdr_bytes, dtype=np.int32, count=1)[0]
 
         # Report on contents of header
         print("=============================================================")
@@ -27,8 +38,11 @@ class dcd_trajectory:
         print("Number of timesteps between snapshots    : ", icntrl[2])
         print("Total number of snapshots in dcd file    : ", icntrl[3])
         print("DCD in format for Charmm version number  : ", icntrl[19]/10.0)
+        print("Number of atoms in each snapshot         : ", self.num_atoms)
         if icntrl[10] == 1:
             print("Header reports presence of unit cell information")
+        print("DCD Title :", self.dcd_title)
+        
 
 
     def read_next_fortran_record(self, dcdfile):
@@ -40,6 +54,7 @@ class dcd_trajectory:
         inbuffer = dcdfile.read(4)  
         pre_len = int()
         pre_len = pre_len.from_bytes(inbuffer, sys.byteorder)
+        
         #print("Length of next record :", pre_len)
 
         # Read that many bytes
